33d337191a515952cbcf6ed6d6f42190cf81cd5c
WhiteSnow00 <kazenosakura100@gmail.com>
2025-07-26 10:56:45 +0700

    QR

--- src/components/floating-support-widget.tsx
+++ src/components/floating-support-widget.tsx
@@ -153,7 +153,7 @@ export function FloatingSupportWidget({ className }: FloatingSupportWidgetProps)
           <div className="relative bg-white rounded-xl p-4 shadow-inner border border-gray-100">
             <div className="relative w-full aspect-square max-w-[200px] mx-auto">
               <img
-                src="/images/qr.png"
+                src="/qr.bmp"
                 alt="QR Code ủng hộ nhóm dịch"
                 width={200}
                 height={200}
