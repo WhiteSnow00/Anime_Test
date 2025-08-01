6b63f81069177c0e566aae8374c73d7c97da4cc9
WhiteSnow00 <kazenosakura100@gmail.com>
2025-08-01 22:49:46 +0700

    Update thumbnail

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,8 +14,8 @@ export const metadata: Metadata = {
     images: [
       {
         url: '/images/thumb.jpg',
-        width: 1200,
-        height: 630,
+        width: 1536,
+        height: 1099,
         alt: 'Web coi anime thay gdrive!',
       },
     ],
