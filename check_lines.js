323a015c99454bce29c64f39e67e1e24a23706b5
WhiteSnow00 <kazenosakura100@gmail.com>
2025-12-13 18:40:37 +0700

    update route

--- check_lines.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-const path = 'src/data/anime.ts';
-const content = fs.readFileSync(path, 'utf8');
-const lines = content.split('\n');
-console.log(lines[61]); // Line 62 (0-indexed 61)
-console.log(lines[205]); // Line 206 (Episode 13)
