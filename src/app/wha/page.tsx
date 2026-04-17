5848054c6b2fef20bcf60cd5bc3662f80dab04b1
hian699 <dna53611@gmail.com>
2026-04-17 11:35:58 +0700

    Comment out AnimePage component in WhaPage

--- src/app/wha/page.tsx
+++ src/app/wha/page.tsx
@@ -1,10 +1,9 @@
 import AnimePage from "@/components/anime-page";
-import { animeWhaData } from "../../data/anime-wha";
 
 export default function WhaPage() {
   return (
     <main>
-      <AnimePage animeData={animeWhaData} animeSlug="wha" />
+      {/* <AnimePage animeData={animeWhaData} animeSlug="wha" /> */}
     </main>
   );
 }
